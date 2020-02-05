a516c26eb128b79d39c066b914622faadab81e02
DNVIC <dnvicbusiness@gmail.com>
2020-02-05 14:33:36 +0000

    added init variables

--- bot.py
+++ bot.py
@@ -1,13 +1,31 @@
 import discord
+import asyncio
+
 
 client = discord.Client()
-#guild = client.get_guild("id")
-#notif = guild.get_role("id")
+#myguild = await client.fetch_guilds(limit = 150)#270723825417584640
+#async for guild in myguild:
+#    print(guild.name)
+#notif = myguild.get_role(409133998195867650)
+
+guild = 0
+notif = 0
+
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    guild = client.get_guild(270723825417584640)
+    notif = guild.get_role(409133998195867650)
+
+
+
+#async for guild in client.fetch_guilds(limit=150):
+ #   print(guild.name)
+
+
+
 
 @client.event
 async def on_message(message):
@@ -17,9 +35,9 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-#@client.event
-#async def on_react(reaction, user):
-    
+@client.event
+async def on_reaction_add(reaction, user):
+    message = reaction.message
     
 file = open('../token.tkn', 'r')
 for line in file:
